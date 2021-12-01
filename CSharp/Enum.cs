e6aa7c4f7793751747bf1283f7f8906039d72d2e
teeagohenrique <teeagomelo@gmail.com>
2021-12-01 18:02:26 -0300

    CSharp e DotNet

---
+++ CSharp/Enum.cs
@@ -0,0 +1,12 @@
+﻿namespace Enum
+{
+    enum Pessoas
+    {
+        Thiago,
+        Giovanna,
+        Julia,
+        João,
+        Gustavo,
+        Mariana
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Primeiro;
+using Classes;
+using Interface;
+using Enum;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoa1 = (Pessoas)0;
+            Pessoas pessoa2 = Pessoas.João;
+            Pessoas pessoa3 = (Pessoas)5;
+
+            Animal animal = new Animal();
+
+            animal.Nome = "Rex";
+            animal.NomeDono = "Paulo";
+            animal.Especie = "Cachorro";
+
+            Pessoa person = new Pessoa();
+
+            person.Nome = "Thiago";
+            person.Idade = 18;
+            person.Estado = "SP";
+
+            var person2 = new Pessoa();
+
+            person2.Nome = "Julia";
+            person2.Idade = 23;
+            person2.Estado = "MG";
+
+            var classe = new Classe();
+            var classe2 = new Segundo.Classe();
+
+            Console.WriteLine(pessoa3);
+        }
+    }
+}
